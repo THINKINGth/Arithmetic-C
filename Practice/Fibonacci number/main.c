b17bf142848ece08dd8210bc2c97709954efe475
Thinker <ChasingIn@outlook.com>
2018-10-25 23:57:09 +0800

    update recursive algorithm

--- Practice/Fibonacci number/main.c
+++ Practice/Fibonacci number/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#define N 100
 /*
  * Computing The Fibonacci Number
  */
 int FibR(int n);
 int FibI(int n,int a,int b,int count);
-//int FibRi(int n,int a[n]);
+int FibRi(int n,int a[n]);
 int main() {
     int n;
     scanf("%d",&n);
-    int a[n];
+    int a[N]= {0};
+    printf("%d\n",FibRi(n,a));
     printf("%d\n",FibI(n,1,1,1));
     printf("%d\n",FibR(n));
     return 0;
@@ -32,3 +34,13 @@ int FibI(int n,int a,int b,int count){
 /*
  * Improved iterative
  */
+int FibRi(int n,int a[n]){
+    if((n == 1 )||(n == 2)){
+        a[n - 1] = 1;
+        return 1;
+    }
+    if(a[n - 1] == 0){
+        a[n - 1] = FibRi(n - 1,a) + FibRi(n - 2,a);
+    }
+    return a[n - 1];
+}
